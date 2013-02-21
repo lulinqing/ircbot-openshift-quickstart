986314826c0ba03c940ec90e298dd3ef0d7990bb
Linqing Lu <lilu@redhat.com>
2013-02-21 18:10:30 +0800

    1

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 #!/bin/env node
 
+// Nodefly monitor
 require('nodefly').profile(
     '909e2fee-c15f-4781-b4ef-c9a72153634f',
     process.env.OPENSHIFT_APP_DNS || 'localhost'
 );
+
+// IRC bot
+var irc = require('irc');
+var bot = new irc.Client('chat.freenode.net', process.env.OPENSHIFT_APP_NAME || 'ircbot', {
+    channels: ['#Linqing', '#botwar'],
+    port: 8001,
+    debug: true
+});
+
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('Know any good jokes?') > -1
+      || message.indexOf('good joke') > -1
+    ) {
+        bot.say(to, 'Knock knock!');
+    }
+});
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('who is there?') > -1
+      || message.indexOf("who's there?") > -1
+      || message.indexOf("Who's there?") > -1
+      || message.indexOf("Who is there?") > -1
+      )
+    {
+        bot.say(to, 'Doris');
+    }
+});
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('Doris who?') > -1
+      || message.indexOf("doris who?") > -1
+     )
+    {
+        bot.say(to, "Doris locked, that's why I'm knocking!");
+    }
+});
+
 
 //  OpenShift sample Node application
 var express = require('express');
