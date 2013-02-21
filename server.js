94754ebdf85bf75230ecedae763bc628e259d5d3
Linqing Lu <lulinqing@gmail.com>
2013-02-21 19:10:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // IRC bot
 var irc = require('irc');
 var bot = new irc.Client('chat.freenode.net', process.env.OPENSHIFT_APP_NAME || 'ircbot', {
-    channels: ['#Linqing', '#botwar'],
+    channels: ['#botzoo', '#botwar'],
     port: 8001,
     debug: true
 });
